693d3a6fe31a38d68e964d6686e2bf0ccf7fcc49
Senthilnathan G <bornspy@gmail.com>
2019-11-18 11:18:16 +0530

    Test

--- hr_shift/models/hr_contract.py
+++ hr_shift/models/hr_contract.py
@@ -8,6 +8,36 @@ from odoo.exceptions import UserError, ValidationError
 
 _logger = logging.getLogger(__name__)
 
+
+class EmployeeShiftRun(models.Model):
+    _name = 'employee.shift.run'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+
+    def _get_current_company(self):
+        return self.env['res.company']._company_default_get()
+
+    company_id = fields.Many2one(string="Current Company", comodel_name='res.company', default=_get_current_company)
+    name = fields.Char('Name', required=True, readonly=True, states={'draft': [('readonly', False)]})
+    start_date = fields.Datetime(string="Date From", required=True, readonly=True,  states={'draft': [('readonly', False)]})
+    end_date = fields.Datetime(string="Date To", required=True, readonly=True, states={'draft': [('readonly', False)]})
+    employee_shift_ids = fields.One2many('hr.employee.shift', 'shift_run_id', string='Employee', ondelete='cascade', index=True, copy=False, readonly=True)
+    hr_shift = fields.Many2one('resource.calendar', string="Shift", required=True, readonly=True, states={'draft': [('readonly', False)]})
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('assigned', 'Assigned')], 'shift', default='draft', required=True)
+
+    @api.multi
+    def close_shift_run(self):
+        for shift in self.employee_shift_ids:
+            shift.close_shift()
+        self.write({'state':'assigned'})
+
+    @api.multi
+    def draft_shift_run(self):
+        self.employee_shift_ids.write({'state':'draft'})
+        self.write({'state':'draft'})
+
+
 class HREmployeeShift(models.Model):
     _name = 'hr.employee.shift'
     _description = 'Employee Shift'
@@ -16,6 +46,7 @@ class HREmployeeShift(models.Model):
     def _get_current_company(self):
         return self.env['res.company']._company_default_get()
 
+    shift_run_id = fields.Many2one('employee.shift.run', 'Batch', states={'draft': [('readonly', False)]})
     company_id = fields.Many2one(string="Current Company", comodel_name='res.company', default=_get_current_company)
     name = fields.Char('Name', required=True, compute='compute_no_of_days', readonly=True, states={'draft': [('readonly', False)]})
     start_date = fields.Datetime(string="Date From", required=True, readonly=True, states={'draft': [('readonly', False)]})
@@ -25,7 +56,6 @@ class HREmployeeShift(models.Model):
     state = fields.Selection([
         ('draft', 'Draft'),
         ('assigned', 'Assigned')], 'Type', default='draft', required=True)
-    shift_run_id = fields.Many2one('employee.shift.run', 'Batch', states={'draft': [('readonly', False)]})
     no_of_days = fields.Float('No.of Days', compute='compute_no_of_days', store=True)
 
     @api.multi
@@ -46,7 +76,7 @@ class HREmployeeShift(models.Model):
     @api.multi
     def schedule_shift(self):
         for employee in self.env['hr.employee'].search([('active','=',True)]):
-            over_lap1 = self.env['employee.shift'].search([('employee_id', '=', employee.id), ('start_date', '<=', fields.Datetime.now()), ('end_date', '>=', fields.Datetime.now())])
+            over_lap1 = self.env['hr.employee.shift'].search([('employee_id', '=', employee.id), ('start_date', '<=', fields.Datetime.now()), ('end_date', '>=', fields.Datetime.now())])
             _logger.info(over_lap1)
             if over_lap1:
                 employee.write({'resource_calendar_id':over_lap1.hr_shift.id})
@@ -60,35 +90,6 @@ class HREmployeeShift(models.Model):
                 line.no_of_days = (d2 - d1).days + 1
                 if line.hr_shift and line.no_of_days:
                     line.name = line.hr_shift.name + ' for ' + str(line.no_of_days) + 'days'
-
-
-class EmployeeShiftRun(models.Model):
-    _name = 'employee.shift.run'
-    _inherit = ['mail.thread', 'mail.activity.mixin']
-
-    def _get_current_company(self):
-        return self.env['res.company']._company_default_get()
-
-    company_id = fields.Many2one(string="Current Company", comodel_name='res.company', default=_get_current_company)
-    name = fields.Char('Name', required=True, readonly=True, states={'draft': [('readonly', False)]})
-    start_date = fields.Datetime(string="Date From", required=True, readonly=True,  states={'draft': [('readonly', False)]})
-    end_date = fields.Datetime(string="Date To", required=True, readonly=True, states={'draft': [('readonly', False)]})
-    employee_shift_ids = fields.One2many('employee.shift', 'shift_run_id', string='Employee', readonly=True, states={'draft': [('readonly', False)]})
-    hr_shift = fields.Many2one('resource.calendar', string="Shift", required=True, readonly=True, states={'draft': [('readonly', False)]})
-    state = fields.Selection([
-        ('draft', 'Draft'),
-        ('assigned', 'Assigned')], 'shift', default='draft', required=True)
-
-    @api.multi
-    def close_shift_run(self):
-        for shift in self.employee_shift_ids:
-            shift.close_shift()
-        self.write({'state':'assigned'})
-
-    @api.multi
-    def draft_shift_run(self):
-        self.employee_shift_ids.write({'state':'draft'})
-        self.write({'state':'draft'})
 
 class HrshiftEmployees(models.TransientModel):
     _name = 'hr.shift.employees'
